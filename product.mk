2e27726bb3ce1d4880aa9b95c6cc0bf1bd2a4749
Shuhao Wu <shuhao@shuhaowu.com>
2019-03-29 10:37:09 -0400

    ABC
    Change-Id: I02115381f9d5f63406316c738017e71a8c04cb97

--- product.mk
+++ product.mk
@@ -1,17 +1,16 @@
 PRODUCT_PACKAGE_OVERLAYS := vendor/extra/overlay/microg
 
 PRODUCT_PACKAGES += \
-	GmsCore \
-	GsfProxy \
-	FakeStore \
-	MozillaNlpBackend \
-	NominatimNlpBackend \
-	com.google.android.maps.jar \
-	FDroid \
-	FDroidPrivilegedExtension \
-	YalpStore \
-	GmsDroidGuard \
-	YahooWeatherProvider
+GmsCore \
+GsfProxy \
+FakeStore \
+MozillaNlpBackend \
+NominatimNlpBackend \
+com.google.android.maps.jar \
+FDroid \
+FDroidPrivilegedExtension \
+# YahooWeatherProvider \
+YalpStore
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey
 PRODUCT_OTA_PUBLIC_KEYS := vendor/extra/keys/releasekey
