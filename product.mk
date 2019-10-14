966db268947dd8604fe446c2fbbc9e2060046b3b
Shuhao Wu <shuhao@shuhaowu.com>
2019-10-14 17:14:44 -0400

    UPDATE2

--- product.mk
+++ product.mk
@@ -1,19 +1,21 @@
-# PRODUCT_PACKAGE_OVERLAYS := vendor/extra/overlay/microg
-#
 PRODUCT_PACKAGES += \
   FDroid \
   FDroidPrivilegedExtension
 
+# PRODUCT_PACKAGE_OVERLAYS := vendor/extra/overlay/microg
+
+# PRODUCT_PACKAGES += \
+# FDroid \
+# FDroidPrivilegedExtension
 # GmsCore \
 # GsfProxy \
 # FakeStore \
 # MozillaNlpBackend \
 # NominatimNlpBackend \
 # com.google.android.maps.jar \
-
 # YahooWeatherProvider \
 # YalpStore
 
-# PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey
-# PRODUCT_OTA_PUBLIC_KEYS := vendor/extra/keys/releasekey
-# PRODUCT_EXTRA_RECOVERY_KEYS := vendor/extra/keys/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey
+PRODUCT_OTA_PUBLIC_KEYS := vendor/extra/keys/releasekey
+PRODUCT_EXTRA_RECOVERY_KEYS := vendor/extra/keys/releasekey
